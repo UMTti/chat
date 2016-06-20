b01a55770542668d324de5e582cc64ab21249d50
UMTti <toivanenpihla@gmail.com>
2016-06-20 14:08:21 +0300

    huhhu

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resource :istunto, only: [:new, :create, :destroy] 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'users#index'
+   get 'signout', to: 'istuntos#destroy'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
